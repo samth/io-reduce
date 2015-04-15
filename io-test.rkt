69bfb4fb5496abcad765daed8189cc66c1f9484a
Sam Tobin-Hochstadt <samth@ccs.neu.edu>
2015-04-15 18:05:20 +0100

    up

--- io-test.rkt
+++ io-test.rkt
@@ -1,6 +1,6 @@
 #lang racket
 
-(require racket/async-channel file/gunzip)
+(require file/gunzip)
 
 ;; This procedure must create a separate thread because clients may read
 ;; from the input port lazily, but the transform procedure eagerly reads
@@ -11,35 +11,32 @@
 ;; send it to a shared channel, and hang onto it to be rereaised in the
 ;; client thread when the client reaches that point in the input.
 
-(struct filter-error (value))
 
-(define (make-filter-input-port/debug transform in)
-  (let-values ([(pipe-input pipe-output) (make-pipe)])
-    (let* ([chan (make-channel)])
-      (thread (lambda ()
-                (with-handlers ([exn? (lambda (exn) (close-output-port pipe-output))])
-                  (transform in pipe-output))))
-      (make-input-port (object-name in)
-                       (lambda (buffer) (read-bytes-avail!* buffer pipe-input))
-                       #f
-                       (lambda () (close-input-port pipe-input))))))
+[define file-in (open-input-file "broken.zip")]
+[define-values (pipe-input pipe-output) (make-pipe)]
+(thread (lambda ()
+          (with-handlers ([exn? (lambda (exn) 
+                                  (printf "pos: ~a\n" (file-position file-in))
+                                  (close-output-port pipe-output))])
+            (inflate file-in pipe-output)
+            (close-input-port pipe-input)
+            (close-output-port pipe-output))))
+[define filter-in 
+  (make-input-port #f
+                   (lambda (buffer)
+                     (define r (read-bytes-avail!* buffer pipe-input))
+                     (printf "read: ~a ~a ~a\n" r (file-position file-in) (file-position filter-in))
+                     r)
+                   #f
+                   (lambda () (close-input-port pipe-input)
+                           (close-output-port pipe-output)))]
 
+(file-position file-in #x26)
 
-;; start-script : (union path string) -> running-script
-;; NOTE: *only* works with big.zip and broken.zip
-(define (start-script path)
-  (let* ([history (make-async-channel 100)]
-         [file-in (open-input-file path)]
-         [filter-in (make-filter-input-port/debug inflate file-in)])
-    (file-position file-in #x26)
-    (thread
-     (lambda ()
-       (let loop ()
-         (cond
-           [(read-line filter-in 'any)
-            => (lambda (line)
-                 (unless (eof-object? line)
-                   (loop)))]))))))
-
-(thread-wait (start-script "broken.zip"))
+(thread-wait   
+ (thread
+  (lambda ()
+    (let loop ()
+      (define d (read-line filter-in 'any))
+      (unless (eof-object? d) (loop))))))
 
